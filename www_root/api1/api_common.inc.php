abe9772fc7af160e4fbac0f9aeeda454913ffb5c
hanshenrik <divinity76@gmail.com>
2017-06-30 13:02:41 +0200

    move base64url_* to api_common

--- www_root/api1/api_common.inc.php
+++ www_root/api1/api_common.inc.php
@@ -16,3 +16,9 @@ function p_verify(string $plain, string $hash): bool {
 	$plain = base64_encode ( $plain );
 	return password_verify ( $plain, $hash );
 }
+function base64url_encode($data) {
+	return rtrim ( strtr ( base64_encode ( $data ), '+/', '-_' ), '=' );
+}
+function base64url_decode($data) {
+	return base64_decode ( str_pad ( strtr ( $data, '-_', '+/' ), strlen ( $data ) % 4, '=', STR_PAD_RIGHT ) );
+}
--- www_root/api1/put/index.php
+++ www_root/api1/put/index.php
@@ -127,12 +127,6 @@ function h_string(string $str): string {
 	$hash = hash ( 'tiger160,4', $str, true );
 	return $hash;
 }
-function base64url_encode($data) {
-	return rtrim ( strtr ( base64_encode ( $data ), '+/', '-_' ), '=' );
-}
-function base64url_decode($data) {
-	return base64_decode ( str_pad ( strtr ( $data, '-_', '+/' ), strlen ( $data ) % 4, '=', STR_PAD_RIGHT ) );
-}
 class Response {
 	public $status_code = 1;
 	public $message = 'unknown error';
